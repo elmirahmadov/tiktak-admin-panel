277c61134ade46b3576d39d603cea66c6cfade60
Tural Babirov <tural.babirov@mail.com>
2025-06-22 12:23:12 +0400

    Compaign page done

--- app/common/helpers/formatDate.ts
+++ app/common/helpers/formatDate.ts
@@ -1,6 +1,9 @@
 export const formatDate = (isoDate: string | null): string => {
    if (!isoDate) return "Məlumat yoxdur";
 
+	console.log(isoDate);
+
+
    const date = new Date(isoDate);
    const day = String(date.getDate()).padStart(2, "0");
    const month = String(date.getMonth() + 1).padStart(2, "0");
